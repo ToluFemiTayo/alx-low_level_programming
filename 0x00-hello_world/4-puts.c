2622a33b31cc96c8d115fc3208fcb50dea750e4e
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-03-17 02:21:10 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,2 @@
-#include <stdio.h>
-/**
- * main - A program that prints a line with puts funtion
- * Return: 0 (Successful!)
- */
-in main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+#!/bin/bash
+gcc 
