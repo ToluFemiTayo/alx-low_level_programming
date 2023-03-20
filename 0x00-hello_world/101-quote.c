05100689dca7a0efb9dcecbb9da5e43fb137255c
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-03-20 09:48:49 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - A c program that prints a line to the standard error
  *
- * Return: 1 (Success)
+ * Return: 1 (error)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
 	return (1);
 }
