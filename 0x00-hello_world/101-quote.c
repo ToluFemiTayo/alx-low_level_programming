00f734e84b70ea54500a61177a19cf67de5606f0
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-03-20 18:13:39 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+	char quo[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
 	return (1);
 }
