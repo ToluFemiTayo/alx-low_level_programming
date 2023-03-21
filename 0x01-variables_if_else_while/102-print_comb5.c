c5cfa84c9f9279b405803303251e8057779b0dd0
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-03-20 20:05:18 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0 (Success)
+ */
+
+intt main(void)
+{
+	int i;
+	int j;
+	
+	for (i = 0 ; i < 100 ; i++)
+	{
+		for (j = 0 ; j < 100 ; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10)+ '0');
+				putchar(' ');
+				putchar((j / 10) + '10');
+				putchar((j % 10) + '10');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
