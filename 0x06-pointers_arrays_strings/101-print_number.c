633b17b1a1bb4c8377be6a2705d3461d5a8a7875
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-03-30 13:49:22 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,18 @@
 
 void print_number(int n)
 {
-	unsigned int nl;
+	unsigned int n1;
 
-	nl = n;
+	n1 = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		nl = -n;
+		n1 = -n;
 	}
-	if (nl / 10 != 0)
+	if (n1 / 10 != 0)
 	{
-		print_number(nl / 10);
+		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
