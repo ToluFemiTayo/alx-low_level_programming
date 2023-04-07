dea969fd788bb7c81d8dd96d8821d137dc9de1af
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-04-07 06:30:57 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
+int check_pal(char *s, int i, int len);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
