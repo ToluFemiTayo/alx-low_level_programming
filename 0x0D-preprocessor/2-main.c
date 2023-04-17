e98913fde903a1fcea8789a5380aaf5f74c05364
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-04-17 10:15:52 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdiio.h>
+
+/**
+ * main - program that prints the name of the file
+ * it was complied from
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf"(%s\n", __FILE__);
+	return (0);
+}
