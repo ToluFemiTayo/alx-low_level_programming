eb554c5d2992754660e6338e66650c7719ae00a9
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-04-20 08:45:44 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,7 +4,7 @@
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_string(const *separator, const unsigned int n, ...);
-void print_all(const char * conts format, ...);
+void print_strings(const *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
