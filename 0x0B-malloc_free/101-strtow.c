d6994d1f6b545f42ab3be2b3e2f8e430ece8fa9f
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-04-26 20:41:24 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -46,7 +46,7 @@ char **strtow(char *str)
 	if (words == 0)
 		return (NULL);
 
-	matrix = (char *) malloc(sizeof(char*) * (words + 1));
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
 
