2af738a74a0a8236124ceeccdd38cb44deb5d023
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-04-27 11:15:41 -0700

    0x12. C - Singly linked lists tasks

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Function that returns number of elements in a linked list
+ * @h: Pointer to the list_t list
+ *
+ * Return: Number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
