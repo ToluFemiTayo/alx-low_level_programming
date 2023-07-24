c80df956c8bfb35aeb92887c12b66b9cf22bd695
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-07-24 15:33:33 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - This prints binary equivalent of a dec numbr
+ * @n: Numbr to print in binary.
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	int k, count = 0;
+	unsigned long int currnt;
+
+	for (k = 63; k >= 0; k--)
+	{
+		currnt = n >> k;
+
+		if (currnt & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
