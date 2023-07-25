8b7b17f6aac3ff10f2df1701c50ca59107ff3d6f
ToluFemiTayo <omodarafemi2123@gmail.com>
2023-07-25 04:59:48 -0700

    5-flip_bits.c, Task-5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - This counts numbr of bits to be changed to
+ * get from one numbr to another.
+ * @n: The first numbr.
+ * @m: The second numbr.
+ *
+ * Return: The numbr of bits to be changed.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int k, count = 0;
+	unsigned long int curnt;
+	unsigned long int exclusv = n ^ m;
+
+	for (k = 63; k >= 0; k--)
+	{
+		curnt = exclus >> k;
+		if (curnt & 1)
+			count++;
+	}
+
+	return (count);
+}
